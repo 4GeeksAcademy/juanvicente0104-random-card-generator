c3d797be87549bd5d1c18a14ac1050e9e02c2567
Juan Guerrero <juanvicente0104@gmail.com>
2024-01-29 19:48:19 +0000

    working on the random card

--- src/app.js
+++ src/app.js
@@ -4,8 +4,39 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { mapToStyles } from "@popperjs/core/lib/modifiers/computeStyles";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //console.log("Hello Rigo from the console!");
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let figures = [
+    { s: "♦", color: "red" },
+    { s: "♥", color: "red" },
+    { s: "♠", color: "black" },
+    { s: "♣", color: "black" }
+  ];
+
+  let number = numbers[Math.floor(Math.random() * 13)];
+  let figure = figures[Math.floor(Math.random() * 4)];
+
+  document.querySelector(".number").innerHTML = number;
+  document.querySelectorAll(".figure").forEach((element, index) => {
+    element.innerHTML = figure.s;
+    element.style.color = figure.color;
+  });
 };
